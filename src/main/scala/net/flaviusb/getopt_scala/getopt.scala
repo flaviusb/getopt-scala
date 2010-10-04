2903b90ca1c6fdb1a30d7be57eb61dd0a4d413d4
Justin Marsh (:flaviusb) <justinius.marsh@gmail.com>
2010-10-05 02:00:10 +1300

    Turn getopt into a trait.
    Have the actual stuff happen in an overridden member cmd: CommandStructure.

--- src/main/scala/net/flaviusb/getopt_scala/getopt.scala
+++ src/main/scala/net/flaviusb/getopt_scala/getopt.scala
@@ -1,5 +1,27 @@
 package net.flaviusb.getopt_scala
 
-class getopt {
+trait getopt {
+  lazy val cmd: CommandStructure = blankCommandStructure
+  def main(arg: Array[String]): Unit = cmd(arg)
+}
+class CommandStructure {
+  lazy val cmd: String => Boolean = ((x: String) => false)
+  lazy val nextLevel: CommandStructure = blankCommandStructure
+  def apply(arg: String) = cmd
+  def apply(foo: Array[String]): Unit = {
+    var arg = foo
+    var not_processed = false
+    while(!not_processed) {
+      not_processed = true;
+      arg = arg.filter(x => { val f = cmd(x); not_processed &&= f; f })
+    }
+    if (arg.length > 0)
+      nextLevel(arg)
+  }
+}
+object blankCommandStructure extends CommandStructure {
+  override def apply(arg: Array[String]): Unit = {
+    // Do nothing
+  }
 
 }
